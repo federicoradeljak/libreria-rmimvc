65193faf054031798933e04767491cf47072a42c
federico.radeljak <federico.radeljak@gmail.com>
2017-11-19 12:48:12 -0300

    creada la excepción personalizada para la librería

---
+++ src/ar/edu/unlu/mvcrmi/MVCRMIException.java
@@ -0,0 +1,13 @@
+package ar.edu.unlu.mvcrmi;
+
+/**
+ * Excepción que es lanzada por la librería MVCRMI por problemas al buscar y registrar modelo/controlador en los registros RMI.
+ * También podrá ser utilizada en el futuro para informar otros errores.
+ */
+public class MVCRMIException extends Exception {
+	private static final long serialVersionUID = 8585722747207599218L;
+
+	public MVCRMIException(String mensaje) {
+		super(mensaje);
+	}
+}
